7d5538c56626199d104df59cb2da58c6422e1311
Marcelino Rodríguez Cancio <marcelinorc@uclv.edu.cu>
2014-03-18 10:55:22 -0430

    - All files firts commit

---
+++ src/main/java/fr/inria/juncoprovider/Junco4TestChecker.java
@@ -0,0 +1,7 @@
+package fr.inria.juncoprovider;
+
+/**
+ * Created by marcel on 16/03/14.
+ */
+public class Junco4TestChecker {
+}
---
+++ src/test/java/fr/inria/juncoprovider/Junco4ProviderTest.java
@@ -0,0 +1,14 @@
+package fr.inria.juncoprovider;
+
+import org.junit.Test;
+
+/**
+ * Created by marcel on 16/03/14.
+ */
+public class Junco4ProviderTest {
+
+    @Test
+    public void testNothing() {
+
+    }
+}
